e2fd4d73ae05472ac3ce8bfff4ed3cff69a8fc5e
Franktian <tianyawen201209@hotmail.com>
2015-01-30 10:14:53 -0500

    Created the alias script

---
+++ alias.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+git config --global alias.co checkout
+git config --global alias.ci commit
+git config --global alias.st status
+git config --global alias.br branch
+git config --global alias.mr merge
+git config --global alias.rb rebase
+git config --global alias.po "push origin"
+git config --global alias.pl "pull origin"
+git config --global alias.pu "pull upstream"
+git config --global alias.alias "config --get-regexp ^alias\." # This is for list all aliases
+
+echo "List all configured aliases"
+git alias
