9446938f04f01585fb14987b897871087ca3cbe2
Franktian <tianyawen201209@hotmail.com>
2015-03-10 06:34:42 -0700

    Include command for cache credentials

--- alias.sh
+++ alias.sh
@@ -9,6 +9,8 @@ git config --global alias.po "push origin"
 git config --global alias.pl "pull origin"
 git config --global alias.pu "pull upstream"
 git config --global alias.alias "config --get-regexp ^alias\." # This is for list all aliases
+# Cache the credentials for 10 hours the next time you input them
+git config --global alias.cache "config --global credential.helper 'cache --timeout=36000'"
 
 echo "List all configured aliases"
 git alias
